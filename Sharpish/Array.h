f7b4625a45a38821e4989f96ab6482f3d9e0b666
Evan Lang <elang@valorem.com>
2020-07-21 01:18:57 -0700

    Documented Array<T>, com_ptr<T>, and weak_ptr<T>

--- Sharpish/Array.h
+++ Sharpish/Array.h
@@ -37,8 +37,9 @@ namespace CS
     class Array final
     {
     public:
-        typedef T_RAW(T) ElementType;
+        /** Element container type. For ComObjects, this com_ptr<T>. For other types, it's just T. */
         typedef T_STORE(T) ElementContainerType;
+        typedef T_RAW(T) ElementType;
         typedef T_STORE(T) value_type;
 
         /// Constructs a null array
@@ -52,7 +53,7 @@ namespace CS
          * 
          * \param size The number of elements of the desired array.
          */
-        Array(size_t size) : _ptr(new T_STORE(T)[size]), _mgr(_ptr, std::default_delete<T_STORE(T)[]>()), _length(size) { }
+        Array(size_t size) : _ptr(new ElementContainerType[size]), _mgr(_ptr, std::default_delete<ElementContainerType[]>()), _length(size) { }
 
         /**
          * \brief Constructs an array that wraps preallocated memory.
@@ -62,14 +63,14 @@ namespace CS
          * \param wrapped The memory to wrap. Note that for reference-counted types this must be `com_ptr<T>*`
          * \param size The number of elements in the array.
          */
-        Array(T_STORE(T)* wrapped, size_t size) : _ptr(wrapped), _length(size) { }
+        Array(ElementContainerType* wrapped, size_t size) : _ptr(wrapped), _length(size) { }
 
         /**
          * \brief Constructs an array from an initializer list..
          * 
          * \param lst The list of values to initialize the array with.
          */
-        Array(std::initializer_list<T> lst) : _ptr(new T_STORE(T)[lst.size()]), _length(lst.size())
+        Array(std::initializer_list<T> lst) : _ptr(new ElementContainerType[lst.size()]), _length(lst.size())
         {
             int i = 0;
             for (auto& x : lst)
@@ -89,7 +90,7 @@ namespace CS
          * \param end Iterator after the last element of the array.
          */
         template<typename Iter>
-        Array(const Iter& start, const Iter& end) : _length(end - start), _ptr(new T_STORE(T)[_length]), _mgr(_ptr, std::default_delete<T_STORE(T)[]>())
+        Array(const Iter& start, const Iter& end) : _length(end - start), _ptr(new ElementContainerType[_length]), _mgr(_ptr, std::default_delete<ElementContainerType[]>())
         {
             int i = 0;
             for (auto iter = start; iter != end; iter++)
@@ -99,31 +100,43 @@ namespace CS
             }
         }
 
+        /**  Assigns an array reference. */
         Array<T>& operator =(const Array<T>& rhs) { _ptr = rhs._ptr; _mgr = rhs._mgr; _length = rhs._length; return *this; }
+        /**  Assigns an array reference to null. */
         Array<T>& operator =(nullptr_t) { _ptr = nullptr; _mgr.reset(); _length = 0; return *this; }
 
-        /// Indexer to an array element.
-        T_STORE(T)& operator [](int index) const { return _ptr[index]; }
-        T_STORE(T)& operator [](short index) const { return _ptr[index]; }
-        T_STORE(T)& operator [](int64_t index) const { return _ptr[index]; }
-        T_STORE(T)& operator [](unsigned int index) const { return _ptr[index]; }
-        T_STORE(T)& operator [](unsigned short index) const { return _ptr[index]; }
-        T_STORE(T)& operator [](uint64_t index) const { return _ptr[index]; }
+        /** Indexer to an array element.. */
+        ElementContainerType& operator [](int index) const { return _ptr[index]; }
+        ElementContainerType& operator [](short index) const { return _ptr[index]; }
+        ElementContainerType& operator [](int64_t index) const { return _ptr[index]; }
+        ElementContainerType& operator [](unsigned int index) const { return _ptr[index]; }
+        ElementContainerType& operator [](unsigned short index) const { return _ptr[index]; }
+        ElementContainerType& operator [](uint64_t index) const { return _ptr[index]; }
 
         typedef T_STORE(T)* iterator;
         typedef const T_STORE(T)* const_iterator;
 
+        /**  Gets an iterator at the beginning of the array. */
         iterator begin() { return _ptr; }
+        /**  Gets a const iterator at the beginning of the array. */
         const_iterator begin() const { return _ptr; }
+        /**  Gets an iterator after the end of the array. */
         iterator end() { return _ptr + _length; }
+        /**  Gets a const iterator after the end of the array. */
         const_iterator end() const { return _ptr + _length; }
+        /**  Gets the size of the array. */
         size_t size() const { return _length; }
+        /**  Gets whether the array has no elements. Also returns true if the array is null. */
         bool empty() const { return _length == 0; }
 
+        /**  Gets whether two `Array<T>`s reference the same array. */
         bool operator ==(const Array<T>& other) const { return _ptr == other._ptr; }
+        /**  Gets whether two `Array<T>`s don't reference the same array. */
         bool operator !=(const Array<T>& other) const { return _ptr != other._ptr; }
 
+        /**  Returns true if the array is non-null. */
         operator bool() const { return !!_ptr; }
+        /**  Returns true if the array is null. */
         bool operator !() const { return !_ptr; }
 
         /** Converts the array to an equivalent `std::vector<T>`. */
@@ -131,8 +144,8 @@ namespace CS
 
     private:
         size_t _length;
-        T_STORE(T)* _ptr;
-        std::shared_ptr<T_STORE(T)> _mgr;
+        ElementContainerType* _ptr;
+        std::shared_ptr<ElementContainerType> _mgr;
 
     };
 }
